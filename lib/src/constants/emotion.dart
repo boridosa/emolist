d158a02e7a80db836ff8432252e1f991b577c17c
boridosa <boridosa12@gmail.com>
2024-02-20 22:58:00 +0900

    🏷️ add Emotion enum

---
+++ lib/src/constants/emotion.dart
@@ -0,0 +1,13 @@
+enum Emotion {
+  love("사랑"),
+  happiness("행복"),
+  anger("분노"),
+  loneliness("외로움"),
+  sadness("슬픔"),
+  hurt("상처"),
+  fear("두려움");
+
+  final String name;
+
+  const Emotion(this.name);
+}
