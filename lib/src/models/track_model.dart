53289bb74c67ec78dc55535b9077b96581dec1d9
boridosa <30467315+boridosa@users.noreply.github.com>
2024-02-21 02:08:43 +0900

    ♻️ rename TrackModel.coverImageUri field to coverUri

--- lib/src/models/track_model.dart
+++ lib/src/models/track_model.dart
@@ -2,13 +2,13 @@ class TrackModel {
   final String id;
   final String title;
   final List<String> artists;
-  final String coverImageUri;
+  final String coverUri;
   final bool liked;
 
   TrackModel.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         title = json['title'],
         artists = List.from(json['artists']),
-        coverImageUri = json['coverImageUri'],
+        coverUri = json['coverUri'],
         liked = json['liked'];
 }
