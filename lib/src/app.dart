0f00782fe86b013a7e682d7e805a5391dc42718f
boridosa <boridosa12@gmail.com>
2024-02-20 19:55:01 +0900

    💬 update app title to Emolist

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,7 +6,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Emolist',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
