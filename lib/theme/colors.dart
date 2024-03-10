ca44e39bb6d35e2ce0561ce9102da00bbb48f57e
keepcalmdog <shinnara11159@sookmyung.ac.kr>
2024-03-10 16:48:10 +0900

    💄 add colors file

---
+++ lib/theme/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const black01 = Color(0xFF252525);
+const black02 = Color(0xFF1E1E1E);
+const black03 = Color(0xFF333333);
+
+const deepGrey01 = Color(0xFF4D4D4D);
+const deepGrey02 = Color(0xFF303030);
+const deepGrey03 = Color(0xFF434343);
+
+const grey01 = Color(0xFF929293);
+const grey02 = Color(0xFF9C9C9C);
+
+const green01 = Color(0xFF1DB854);
+const green02 = Color(0xFF418C52);
