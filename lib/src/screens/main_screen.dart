4757976a8994536e01fbd578d31fb800a864da12
keepcalmdog <shinnara11159@sookmyung.ac.kr>
2024-03-15 13:40:49 +0900

    🔨  remove switch statement for FloatingActionButton on MainScreen

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -24,90 +24,61 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: body,
-      bottomNavigationBar: NavigationBar(
-        backgroundColor: black03,
-        destinations: const [
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.house),
-            label: '홈',
-          ),
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.book),
-            label: '일기',
-          ),
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.headphones),
-            label: '플레이리스트',
-          ),
-        ],
-        selectedIndex: currentRoute.index,
-        onDestinationSelected: (index) {
-          final destination = _getDestination(index);
-          context.go(destination.path);
-        },
-      ),
-      floatingActionButton: switch (currentRoute) {
-        RoutePath.home || RoutePath.diary => FloatingActionButton(
-            onPressed: () async {
-              final DiaryModel? diaryContent = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DiaryWriteScreen(
-                    id: 'your_id_value',
-                    date: DateTime.now(),
-                    content: '',
-                    emotions: const {},
-                    playlist: const [],
-                  ),
+        body: body,
+        bottomNavigationBar: NavigationBar(
+          backgroundColor: black03,
+          destinations: const [
+            NavigationDestination(
+              selectedIcon: FaIcon(
+                FontAwesomeIcons.house,
+                color: Colors.white,
+              ),
+              icon: FaIcon(FontAwesomeIcons.house),
+              label: '홈',
+            ),
+            NavigationDestination(
+              icon: FaIcon(FontAwesomeIcons.book),
+              label: '일기',
+            ),
+            NavigationDestination(
+              icon: FaIcon(FontAwesomeIcons.headphones),
+              label: '플레이리스트',
+            ),
+          ],
+          selectedIndex: currentRoute.index,
+          onDestinationSelected: (index) {
+            final destination = _getDestination(index);
+            context.go(destination.path);
+          },
+          animationDuration: Duration.zero,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final DiaryModel? diaryContent = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DiaryWriteScreen(
+                  id: 'your_id_value',
+                  date: DateTime.now(),
+                  content: '',
+                  emotions: const {},
+                  playlist: const [],
                 ),
-              );
-              if (diaryContent != null) {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            DiaryScreen(newDiaryContent: diaryContent)));
-              }
-            },
-            tooltip: '일기 작성',
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100)),
-            child: const FaIcon(FontAwesomeIcons.pen),
-          ),
-        RoutePath.playlist => FloatingActionButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('멜론 플레이리스트 내보내기'),
-                    content: const Text('선택한 곡을 플레이리스트로 내보내시겠습니까?'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('취소'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('확인'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            tooltip: '플레이리스트 내보내기',
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100)),
-            child: const FaIcon(FontAwesomeIcons.share),
-          ),
-      },
-    );
+              ),
+            );
+            if (diaryContent != null) {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          DiaryScreen(newDiaryContent: diaryContent)));
+            }
+          },
+          tooltip: '일기 작성',
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+          child: const FaIcon(FontAwesomeIcons.pen),
+        ));
   }
 
   RoutePath _getDestination(index) {
